9f5845e5344d87f941a1271c128b61f31438187c
dgucik <danielgucik7@gmail.com>
2026-01-31 17:30:04 +0100

    Add docs for busses

--- src/infrastructure/messaging/command_bus.py
+++ src/infrastructure/messaging/command_bus.py
@@ -4,7 +4,12 @@ from infrastructure.exceptions import HandlerNotRegisteredException
 
 
 class InMemoryCommandBus(CommandBus):
-    """ """
+    """
+    In-memory implementation of the command bus.
+
+    Attributes:
+        _handlers: A dictionary of command types and their handlers.
+    """
 
     def __init__(self) -> None:
         self._handlers: dict[type[Any], CommandHandler[Any]] = {}
@@ -12,9 +17,22 @@ class InMemoryCommandBus(CommandBus):
     def register_handler(
         self, command_type: type[Any], handler: CommandHandler[Any]
     ) -> None:
+        """
+        Register a handler for a command type.
+
+        Args:
+            command_type: The type of command to register the handler for.
+            handler: The handler to register for the command type.
+        """
         self._handlers[command_type] = handler
 
     async def execute(self, command: Any) -> None:
+        """
+        Execute a command.
+
+        Args:
+            command: The command to execute.
+        """
         command_type = type(command)
         handler = self._handlers.get(command_type)
         if handler is None:
--- src/infrastructure/messaging/query_bus.py
+++ src/infrastructure/messaging/query_bus.py
@@ -4,15 +4,35 @@ from infrastructure.exceptions import HandlerNotRegisteredException
 
 
 class InMemoryQueryBus(QueryBus):
+    """
+    In-memory implementation of the query bus.
+
+    Attributes:
+        _handlers: A dictionary of query types and their handlers.
+    """
+
     def __init__(self) -> None:
         self._handlers: dict[type[Any], QueryHandler[Any, Any]] = {}
 
     def register_handler(
         self, query_type: type[Any], handler: QueryHandler[Any, Any]
     ) -> None:
+        """
+        Register a handler for a query type.
+
+        Args:
+            query_type: The type of query to register the handler for.
+            handler: The handler to register for the query type.
+        """
         self._handlers[query_type] = handler
 
     async def execute(self, query: Any) -> Any:
+        """
+        Execute a query.
+
+        Args:
+            query: The query to execute.
+        """
         query_type = type(query)
         handler = self._handlers.get(query_type)
         if handler is None:
