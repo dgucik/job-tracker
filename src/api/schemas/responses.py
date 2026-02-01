eec49d2817ba2f9b1e883073161268c63470cff8
dgucik <danielgucik7@gmail.com>
2026-02-01 15:39:40 +0100

    Add aliases for Responses

--- src/api/schemas/responses.py
+++ src/api/schemas/responses.py
@@ -3,19 +3,29 @@ from pydantic import BaseModel, Field
 
 
 class CompensationListItemResponse(BaseModel):
-    min_salary: int | None
-    max_salary: int | None
-    currency: str | None
-    employment_type: str
+    min_salary: int | None = Field(examples=[5000], alias="minSalary")
+    max_salary: int | None = Field(examples=[8000], alias="maxSalary")
+    currency: str | None = Field(examples=["USD"], alias="currency")
+    employment_type: str = Field(examples=["B2B"], alias="employmentType")
 
 
 class JobApplicationListItemResponse(BaseModel):
     id: UUID
-    company_name: str = Field(min_length=1, max_length=255, examples=["Google"])
-    role_name: str = Field(min_length=1, max_length=255, examples=["Software Engineer"])
-    posting_url: str = Field(examples=["https://www.google.com"])
-    status: str = Field(examples=["APPLIED"])
-    work_model: str = Field(examples=["REMOTE"])
-    work_location: str | None = Field(default=None, examples=["Warsaw"])
-    compensations: list[CompensationListItemResponse] = Field(default_factory=list)
-    notes: str | None = Field(default=None, examples=["Exciting opportunity"])
+    company_name: str = Field(
+        min_length=1, max_length=255, examples=["Google"], alias="companyName"
+    )
+    role_name: str = Field(
+        min_length=1, max_length=255, examples=["Software Engineer"], alias="roleName"
+    )
+    posting_url: str = Field(examples=["https://www.google.com"], alias="postingUrl")
+    status: str = Field(examples=["APPLIED"], alias="status")
+    work_model: str = Field(examples=["REMOTE"], alias="workModel")
+    work_location: str | None = Field(
+        default=None, examples=["Warsaw"], alias="workLocation"
+    )
+    compensations: list[CompensationListItemResponse] = Field(
+        default_factory=list, alias="compensations"
+    )
+    notes: str | None = Field(
+        default=None, examples=["Exciting opportunity"], alias="notes"
+    )
