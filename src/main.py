4a85265bbfe0923d0cad3380edbd32eeafa04377
dgucik <danielgucik7@gmail.com>
2026-02-03 00:32:14 +0100

    Exc handlers

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import logging
 from fastapi import FastAPI
 
+from api.exception_handlers import register_exception_handlers
 from api.routes import router as job_applications_router
 from settings import settings
 
@@ -12,6 +13,7 @@ def create_app() -> FastAPI:
     logger.info("Application started")
     app = FastAPI()
     app.include_router(job_applications_router, prefix="/v1/job-applications")
+    register_exception_handlers(app)
     return app
 
 
